0ec1d45e72c526d2a0f131d58cac7e6d9682cc7f
Ernesto Tagwerker <ernesto@ombushop.com>
2013-01-08 18:05:00 -0300

    Added NEWRELIC_ENABLE=false to test in production.

--- lib/capistrano-resque/capistrano_integration.rb
+++ lib/capistrano-resque/capistrano_integration.rb
@@ -33,7 +33,7 @@ module CapistranoResque
 
         def start_command(queue, pid)
           "cd #{current_path} && RAILS_ENV=#{rails_env} QUEUE=\"#{queue}\" \
-           PIDFILE=#{pid} TERM_CHILD=1 BACKGROUND=yes VERBOSE=1 \
+           PIDFILE=#{pid} TERM_CHILD=1 NEWRELIC_ENABLE=false BACKGROUND=yes VERBOSE=1 \
            #{fetch(:bundle_cmd, "bundle")} exec rake resque:work"
         end
 
